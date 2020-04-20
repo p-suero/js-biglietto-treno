e47a6ad28f54d694ae932aa7a0da4339b2b8bf44
Paolo <paolo.suero@gmail.com>
2020-04-20 21:09:21 +0200

    Definizioni

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ console.log(sconto_under18);
 var sconto_over65 = numero_km * ((0.21 * 40) / 100);
 console.log(sconto_over65);
 
-//creo la condizione per stampare all'interno del html il costo del biglietto
+//creo la condizione per determinare il costo finale del biglietto
 if ((eta_passeggero >= 18) && (eta_passeggero <= 65)) {
     var prezzo_finale = prezzo_base.toFixed(2);
 } else if (eta_passeggero < 18) {
@@ -26,6 +26,8 @@ if ((eta_passeggero >= 18) && (eta_passeggero <= 65)) {
     prezzo_finale = (prezzo_base - sconto_over65).toFixed(2);
 }
 
-// stampo il risultato nella pagina html e lo rendo visibile con l'assegnazione della classe per evitare che la pagina si visualizzi durante l'inserimento dei dati nel prompt
-document.getElementById('final-price').innerHTML= "Il prezzo del tuo biglietto è pari a:" + " " + prezzo_finale;
-document.getElementById("ticket").setAttribute("class", "visible");
+// stampo i risultati nella pagina html e lo rendo visibile con l'assegnazione della classe per evitare che la pagina si visualizzi durante l'inserimento dei dati nel prompt
+document.getElementById('km').innerHTML= numero_km + " " + "km";
+document.getElementById('age').innerHTML= eta_passeggero + " " + "anni";
+document.getElementById('price').innerHTML= prezzo_finale + " " + "euro";
+document.getElementById("card-container").setAttribute("class", "visible");
